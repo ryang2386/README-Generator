30104ae46db7026dadf7b729e284bdb23ec80146
Ryan Gayle <gayler02@nyu.edu>
2025-02-08 18:11:56 -0500

    created a list of questions in questions array

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
 import fs from 'fs';
+import { type } from 'os';
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+    .prompt([
+questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Describe your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage of this project?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are you contribution guidelines?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'tests',
+    },
+] 
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
