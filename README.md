e76566b0023d807430f0e35c26143425689f833c
Ryan Gayle <gayler02@nyu.edu>
2025-02-11 20:12:22 -0500

    updated note in License and Questions sections in README

--- README.md
+++ README.md
@@ -1,28 +1,8 @@
 ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 # r
 
-## Description
-e
+## Table of Contents
 
- ## Installation
-a
-
- ## Usage
-d
-
- ## Contribution
- m
-
- ## Tests
- e
-
- ## License
- MIT
-
- ## Questions
- GitHub: rt - https://github.com/rt
-
- Email: re
 [Description](#Description)
 
 [Installation](#Installation)
@@ -36,3 +16,39 @@ d
 [License](#License)
 
 [Questions](#Questions)
+
+
+## Description
+t
+
+ ## Installation
+r
+
+ ## Usage
+t
+
+ ## Contribution
+ e
+
+ ## Tests
+ r
+
+ ## License
+ MIT
+
+ Copyright © 2024 Ryan Gayle
+
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ ## Questions
+ GitHub: er - https://github.com/er
+
+ Email: we
+
+ I've provided my GitHub and email for any questions regarding this project. From Monday through Friday, I am available to answer any questions between 11 AM and 6 PM. On weekends, please allow me 24 hours to respond. Thank you.
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
-import fs from 'fs';
+import fs, { read } from 'fs';
 import generateMarkdown from './utils/generateMarkdown.js';
 // TODO: Create an array of questions for user input
 let questions = ['What is the title of your project?', 'Describe your project.', 'What are the installation instructions?', 'What is the usage of this project?', 'What are your contribution guidelines?', 'What are the test instructions?'];
 let name = ['title', 'description', 'installation', 'usage', 'contribution', 'tests'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFileSync(fileName, `# ${data.title}\n\n## Description\n${data.description}\n\n ## Installation\n${data.installation}\n\n ## Usage\n${data.usage}\n\n ## Contribution\n ${data.contribution}\n\n ## Tests\n ${data.tests}\n\n ## License\n ${data.license}\n\n ## Questions\n GitHub: ${data.github} - https://github.com/${data.github}\n\n Email: ${data.email}`);
-}
+function writeToFile(fileName, data, markdown) {
+    fs.writeFileSync(fileName, `# ${data.title}\n\n## Table of Contents\n`);
+    for (let i = 2; i < markdown.length; i++) {
+        fs.appendFileSync('README.md', `\n[${markdown[i]}](#${markdown[i]})\n`);
+     }
+     const readMeFile = fs.readFileSync('README.md', 'utf8');
+     fs.writeFileSync('README.md', `${readMeFile}\n\n## Description\n${data.description}\n\n ## Installation\n${data.installation}\n\n ## Usage\n${data.usage}\n\n ## Contribution\n ${data.contribution}\n\n ## Tests\n ${data.tests}\n\n ## License\n ${data.license}\n\n Copyright © 2024 Ryan Gayle\n\n
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n\n ## Questions\n GitHub: ${data.github} - https://github.com/${data.github}\n\n Email: ${data.email}\n\n I've provided my GitHub and email for any questions regarding this project. From Monday through Friday, I am available to answer any questions between 11 AM and 6 PM. On weekends, please allow me 24 hours to respond. Thank you.`);
+    }
 
 // TODO: Create a function to initialize app
 async function init() {
@@ -49,21 +60,18 @@ async function init() {
         answers.license = license.license;
         answers.github = github.github;
         answers.email = email.email;
-        // answers.email = email.email;
-        // console.log(answers);
-        writeToFile('README.md', answers);
+
         const licenseBadge = generateMarkdown.renderLicenseBadge(license.license);
         let markdown = generateMarkdown.generateMarkdown(answers);
-        // markdown = markdown.replace("\n\n", "\n");
+
         markdown = markdown.split("#").join("");
         markdown = markdown.split("\n").join("");
         markdown = markdown.split(" ");
-        // console.log(markdown);
+
+        writeToFile('README.md', answers, markdown);
         const readMe = fs.readFileSync('README.md', 'utf8');
         fs.writeFileSync('README.md', `${licenseBadge}\n${readMe}`);
-        for (let i = 2; i < markdown.length; i++) {
-            fs.appendFileSync('README.md', `\n[${markdown[i]}](#${markdown[i]})\n`);
-        }
+
         // const licenseLink = generateMarkdown.renderLicenseLink(license.license);
         // const licenseSection = generateMarkdown.renderLicenseSection(license.license);
         // console.log(licenseLink);
